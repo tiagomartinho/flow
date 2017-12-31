8f275c05f1fc9f491aec622bf890e31a3ea2a638
Tiago Martinho <tiago@elit.software>
2017-12-31 17:23:56 +0100

    read/write value from CloudKit

--- Core/ValueRepository.swift
+++ Core/ValueRepository.swift
@@ -1,15 +1,49 @@
+import CloudKit
+
 public class ValueRepository: Repository {
 
     var value = 42
+
+    var container: CKContainer {
+        return CKContainer(identifier: "iCloud.com.elit.flow")
+    }
+
+    var database: CKDatabase {
+        return container.privateCloudDatabase
+    }
+
+    var recordID: CKRecordID {
+        return CKRecordID(recordName: "Value")
+    }
 
     public init() {
     }
 
     public func write(_ value: Int) {
+        let record = CKRecord(recordType: "Value", recordID: recordID)
+        record["value"] = value as NSNumber
+        database.save(record) {
+            (record, error) in
+            if let error = error {
+                // Insert error handling
+                return
+            }
+            // Insert successfully saved record code
+        }
         self.value = value
     }
 
     public func load() -> Int {
+        database.fetch(withRecordID: recordID) { (record, error) in
+            if let error = error {
+                // Error handling for failed fetch from public database
+                return
+            }
+            // Display the fetched record
+            if let value = record?["value"] as? Int {
+                self.value = value
+            }
+        }
         return value
     }
 }
