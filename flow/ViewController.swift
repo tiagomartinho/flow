cec9263ecaf1966b0aed50c118c1508df4c47087
Tiago Martinho <tiago@elit.software>
2017-12-31 16:24:06 +0100

    Read/Write value from Repository

--- flow/ViewController.swift
+++ flow/ViewController.swift
@@ -1,25 +1,42 @@
-//
-//  ViewController.swift
-//  flow
-//
-//  Created by Tiago Martinho on 31/12/17.
-//  Copyright © 2017 tm. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var valueTextField: UITextField!
+
+    let repository = ValueRepository()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func write(_ sender: UIButton) {
+        repository.write(Int(valueTextField.text!)!)
+        updateUI()
     }
 
-
+    func updateUI() {
+        valueLabel.text = "\(repository.load())"
+    }
 }
 
+protocol Repository {
+    associatedtype T
+    func load() -> T
+    func write(_ value: T)
+}
+
+class ValueRepository: Repository {
+
+    var value = 42
+
+    func write(_ value: Int) {
+        self.value = value
+    }
+
+    func load() -> Int {
+        return value
+    }
+}
