3439ea2ff629a8a2ff24478ea80a40e6c6339f46
Tiago Martinho <tiago@elit.software>
2018-01-01 19:32:41 +0100

    Add macOS target

---
+++ flowmacos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  flowmacos
+//
+//  Created by Tiago Martinho on 1/1/18.
+//  Copyright © 2018 tm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ flowmacos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  flowmacos
+//
+//  Created by Tiago Martinho on 1/1/18.
+//  Copyright © 2018 tm. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
